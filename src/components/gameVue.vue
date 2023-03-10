2015557ee25ed4f7ef25486963a4b28b2b72682b
yusikerem <kerem.yky@gmail.com>
2023-03-10 11:54:23 +0300

    Funcitons refactorise

--- src/components/gameVue.vue
+++ src/components/gameVue.vue
@@ -19,7 +19,7 @@
       <img width="40" height="40" src="../assets/icons8-apple-64.png" alt="" />
     </div>
     <div
-      :class="{ ' border-boundary grid grid-cols-7': !gameFinished }"
+      :class="{ ' border-boundary grid grid-cols-10': !gameFinished }"
       class="w-full h-full gap-1 border-boundary border-4"
     >
       <div
@@ -48,7 +48,7 @@
         v-else
         :class="{
           'bg-success': cell == 'snake',
-          'bg-red-500': cell == 'apple',
+          'bg-alert': cell == 'apple',
         }"
         class="bg-primary text-white"
         :key="i"
@@ -60,7 +60,7 @@
 <script setup>
 import { ref, onMounted } from "vue";
 let score = 0;
-const cellCount = 49;
+const cellCount = 100;
 let board = ref(Array.from({ length: cellCount }, (_, index) => index));
 let firstPlay = true;
 let direction = "";
@@ -72,33 +72,122 @@ const gameFinished = ref(false);
 onMounted(() => {
   placeSnake();
   placeApple();
-  window.addEventListener("keydown", (e) => {
-    let keyPressed = e.key;
-    switch (keyPressed) {
-      case "w":
-      case "ArrowUp":
-        direction !== "downward" ? (direction = "upward") : undefined;
-        firstPlay = false;
-        break;
-      case "s":
-      case "ArrowDown":
-        direction !== "upward" ? (direction = "downward") : undefined;
-        firstPlay = false;
-        break;
-      case "a":
-      case "ArrowLeft":
-        direction !== "right" ? (direction = "left") : undefined;
-        firstPlay = false;
-        break;
-      case "d":
-      case "ArrowRight":
-        direction !== "left" ? (direction = "right") : undefined;
-        firstPlay = false;
-        break;
-    }
-  });
+  gameIntervalID();
 });
 
+function playAgain() {
+  score = 0;
+  gameFinished.value = false;
+  direction = "";
+  //new empty array
+  board.value = Array.from({ length: cellCount }, (_, index) => index);
+  snakePosition = [2, 1, 0];
+  placeSnake();
+  placeApple();
+}
+
+function placeApple() {
+  // get empty cells
+  let emptyCells = board.value
+    .map((cell) => (cell !== "snake" ? cell : undefined))
+    .filter((cell) => cell !== undefined);
+  // put the apple to random place inside of empty cells
+  appleIndex = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+  board.value[appleIndex] = "apple";
+}
+function placeSnake() {
+  snakePosition.forEach((index) => (board.value[index] = "snake"));
+}
+
+function checkSneakIsEating() {
+  appleIndex === snakePosition[0] ? (feedSnake(), placeApple()) : "";
+}
+function feedSnake() {
+  snakePosition.push(snakePosition[0] + handleDirection);
+  score++;
+}
+function move() {
+  if (direction !== "") {
+    // for example = [2,1,0]
+    let tail = snakePosition.pop();
+    //[2,1]
+    board.value[tail] = tail;
+    snakePosition.unshift(snakePosition[0] + handleDirection());
+    //[3,2,1]
+    placeSnake();
+    checkSneakIsEating();
+    checkSnakeIsEatingTail();
+    checkBoundaries();
+  }
+}
+function handleDirection() {
+  return direction === "right"
+    ? 1
+    : direction === "left"
+    ? -1
+    : direction === "upward"
+    ? -cellsByEdge
+    : direction === "downward"
+    ? +cellsByEdge
+    : 0;
+}
+
+function gameIntervalID() {
+  const gameInterval = setInterval(() => {
+    handleDirection(), move();
+  }, 200);
+  return gameInterval;
+}
+
+function checkSnakeIsEatingTail() {
+  snakePosition.slice(1).includes(snakePosition[0]) ? showResult() : "";
+}
+function showResult() {
+  gameFinished.value = true;
+  clearInterval(gameIntervalID());
+}
+function checkBoundaries() {
+  let head = snakePosition[0];
+  direction === "upward" && head >= 0 - cellsByEdge && head < 0
+    ? showResult()
+    : direction === "downward" &&
+      head < cellCount + cellsByEdge &&
+      head >= cellCount
+    ? showResult()
+    : direction === "left" && (head + 1) % cellsByEdge == 0
+    ? showResult()
+    : direction === "right" && head % cellsByEdge == 0
+    ? showResult()
+    : undefined;
+}
+
+//Event listeners
+window.addEventListener("keydown", (e) => {
+  let keyPressed = e.key;
+  switch (keyPressed) {
+    case "w":
+    case "ArrowUp":
+      direction !== "downward" ? (direction = "upward") : undefined;
+      firstPlay = false;
+      break;
+    case "s":
+    case "ArrowDown":
+      direction !== "upward" ? (direction = "downward") : undefined;
+      firstPlay = false;
+      break;
+    case "a":
+    case "ArrowLeft":
+      direction !== "right" ? (direction = "left") : undefined;
+      firstPlay = false;
+      break;
+    case "d":
+    case "ArrowRight":
+      direction !== "left" ? (direction = "right") : undefined;
+      firstPlay = false;
+      break;
+  }
+});
+//touch listeners for mobile-tablet devices
 document.addEventListener("touchstart", handleTouchStart, false);
 document.addEventListener("touchmove", handleTouchMove, false);
 
@@ -106,10 +195,7 @@ var xDown = null;
 var yDown = null;
 
 function getTouches(evt) {
-  return (
-    evt.touches || // browser API
-    evt.originalEvent.touches
-  ); // jQuery
+  return evt.touches || evt.originalEvent.touches;
 }
 
 function handleTouchStart(evt) {
@@ -150,95 +236,5 @@ function handleTouchMove(evt) {
   /* reset values */
   xDown = null;
   yDown = null;
-}
-
-function playAgain() {
-  clearBoard();
-  appleIndex = undefined;
-  score = 0;
-  gameFinished.value = false;
-  direction = "";
-  board.value = Array.from({ length: cellCount }, (_, index) => index);
-  snakePosition = [2, 1, 0];
-  placeSnake();
-  placeApple();
-}
-function clearBoard() {
-  board.value.forEach((cell) => ((cell = ""), cell));
-}
-
-function placeApple() {
-  let emptyCells = [];
-  board.value.forEach((cell) =>
-    cell !== "snake" ? emptyCells.push(cell) : undefined
-  );
-  appleIndex = emptyCells[Math.round(Math.random() * emptyCells.length)];
-  board.value[appleIndex] = "apple";
-}
-function placeSnake() {
-  snakePosition.forEach((index) => (board.value[index] = "snake"));
-}
-
-function checkSneakIsEating() {
-  appleIndex === snakePosition[0] ? (feedSnake(), placeApple()) : "";
-}
-function feedSnake() {
-  snakePosition.push(snakePosition[0] + handleDirection);
-  score++;
-}
-function move() {
-  if (direction !== "") {
-    // for example = [2,1,0]
-    let tail = snakePosition.pop();
-    //[2,1]
-    board.value[tail] = tail;
-    snakePosition.unshift(snakePosition[0] + handleDirection());
-    //[3,2,1]
-    placeSnake();
-    checkSneakIsEating();
-    checkSnakeIsEatingTail();
-    checkBoundaries();
-  }
-}
-function handleDirection() {
-  return direction === "right"
-    ? 1
-    : direction === "left"
-    ? -1
-    : direction === "upward"
-    ? -cellsByEdge
-    : direction === "downward"
-    ? +cellsByEdge
-    : 0;
-}
-
-function intervalID() {
-  const gameInterval = setInterval(() => {
-    handleDirection(), move();
-  }, 500);
-  return gameInterval;
-}
-console.log(intervalID());
-
-function checkSnakeIsEatingTail() {
-  snakePosition.slice(1).includes(snakePosition[0]) ? showResult() : "";
-}
-function showResult() {
-  gameFinished.value = true;
-  clearInterval(intervalID());
-}
-function checkBoundaries() {
-  let head = snakePosition[0];
-  direction === "upward" && head >= 0 - cellsByEdge && head < 0
-    ? showResult()
-    : direction === "downward" &&
-      head < cellCount + cellsByEdge &&
-      head > cellCount
-    ? showResult()
-    : direction === "left" && (head + 1) % cellsByEdge == 0
-    ? showResult()
-    : direction === "right" && head % cellsByEdge == 0
-    ? showResult()
-    : "";
 }
 </script>
