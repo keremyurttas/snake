c033759474a990a7cf8189130afb77258b03d8af
yusikerem <kerem.yky@gmail.com>
2023-12-11 11:57:16 +0300

    Fix scroll problem on mobile devices

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="mx-auto w-screen h-screen bg-black p-10">
+  <div class="mx-auto w-screen h-screen bg-black p-10 overflow-hidden">
     <h1 class="text-5xl mb-20 text-center text-success">Snake</h1>
-    <game-vue></game-vue>
+    <snake-game></snake-game>
   </div>
 </template>
 <script setup>
 import { defineComponent } from "vue";
-import gameVue from "./components/gameVue.vue";
-defineComponent(gameVue);
+import SnakeGame from "./components/SnakeGame.vue";
+defineComponent(SnakeGame);
 </script>
